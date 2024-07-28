8421c556fa18e568376d36e152f2e4d982957f3c
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-07-28 11:14:45 +0200

    implement win tie conditions
    game fully playable on console

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function Gameboard() {
     const boardWithCellValues = board.map((row) =>
       row.map((cell) => cell.getValue())
     );
-    console.log(boardWithCellValues);
+    console.table(boardWithCellValues);
   };
 
   const checkWin = (row, column) => {
@@ -48,13 +48,12 @@ function Gameboard() {
     for (let i = 0; i < dimension; i++)
       diagonal2.push(board[2 - i][i].getValue());
 
-    let win =
+    return (
       fullRow.every(equals) ||
       fullColumn.every(equals) ||
       diagonal1.every(equals) ||
-      diagonal2.every(equals);
-
-    return win;
+      diagonal2.every(equals)
+    );
   };
 
   return { getBoard, markCell, printBoard, checkWin };
@@ -77,20 +76,24 @@ function GameController(
   playerOneName = "Player One",
   playerTwoName = "Player Two"
 ) {
-  const board = Gameboard();
+  let board = Gameboard();
 
   const players = [
     {
       name: playerOneName,
       token: 1,
+      win: 0,
     },
     {
       name: playerTwoName,
       token: 2,
+      win: 0,
     },
   ];
 
   let activePlayer = players[0];
+  let moveCount = 0;
+  let ties = 0;
 
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -100,6 +103,12 @@ function GameController(
   const printNewRound = () => {
     board.printBoard();
     console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const newMatch = () => {
+    activePlayer = players[0];
+    board = Gameboard();
+    moveCount = 0;
   };
 
   const playRound = (row, column) => {
@@ -112,10 +121,42 @@ function GameController(
     let marked;
     marked = board.markCell(row, column, getActivePlayer().token);
 
-    console.log(board.checkWin(row, column));
-    if (!marked) switchPlayerTurn();
+    if (!marked) {
+      switchPlayerTurn();
+      moveCount++;
+    }
+
+    if (board.checkWin(row, column)) {
+      switchPlayerTurn();
+      activePlayer.win++;
+      console.log(`${activePlayer.name} won the match!`);
+      if (activePlayer.win === 3) {
+        console.log(`${activePlayer.name} is the winner!`.toUpperCase());
+        console.log("Starting a new one...");
+        players[0].win = 0;
+        players[1].win = 0;
+        ties = 0;
+        moveCount = 0;
+      }
+      newMatch();
+    }
+
+    
 
     printNewRound();
+
+    if (moveCount === 9) {
+      console.log("It's a tie!");
+      ties++;
+      newMatch();
+    }
+
+    console.log({
+      player0win: players[0].win,
+      player1win: players[1].win,
+      ties,
+      moveCount,
+    });
   };
 
   return {
