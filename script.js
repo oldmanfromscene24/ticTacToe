1d18e181f49400aef953f2e2ed0b86988fd3d154
oldmanfromscene24 <giancarlo.spiller@gmail.com>
2024-07-30 22:03:57 +0200

    Add ui
    TODO: restart button, display win to ui

--- script.js
+++ script.js
@@ -56,7 +56,15 @@ function Gameboard() {
     );
   };
 
-  return { getBoard, markCell, printBoard, checkWin };
+  const resetBoard = () => {
+      for (let i = 0; i < 3; i++)
+        for (let j = 0; j < 3; j++) {
+      board[i][j].resetToken()
+        }
+
+  }
+
+  return { getBoard, markCell, printBoard, checkWin, resetBoard };
 }
 
 function Cell() {
@@ -65,10 +73,14 @@ function Cell() {
     value = player;
   };
   const getValue = () => value;
+  const resetToken = () => {
+    value = 0;
+  }
 
   return {
     addToken,
     getValue,
+    resetToken,
   };
 }
 
@@ -93,7 +105,7 @@ function GameController(
 
   let activePlayer = players[0];
   let moveCount = 0;
-  let ties = 0;
+  let tieCount = 0;
 
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -107,8 +119,8 @@ function GameController(
 
   const newMatch = () => {
     activePlayer = players[0];
-    board = Gameboard();
     moveCount = 0;
+    board.resetBoard();
   };
 
   const playRound = (row, column) => {
@@ -135,34 +147,76 @@ function GameController(
         console.log("Starting a new one...");
         players[0].win = 0;
         players[1].win = 0;
-        ties = 0;
+        tieCount = 0;
         moveCount = 0;
       }
       newMatch();
     }
 
-    
-
     printNewRound();
 
     if (moveCount === 9) {
       console.log("It's a tie!");
-      ties++;
+      tieCount++;
       newMatch();
     }
-
-    console.log({
-      player0win: players[0].win,
-      player1win: players[1].win,
-      ties,
-      moveCount,
-    });
+    
+    const player1 = document.querySelector(".player1");
+    const player2 = document.querySelector(".player2");
+    const ties = document.querySelector(".ties");
+    const moves = document.querySelector(".moves");
+    
+    player1.textContent = `${players[0].name}: ${players[0].win}`;
+    player2.textContent = `${players[1].name}: ${players[1].win}`;
+    ties.textContent = `Ties: ${tieCount}`;
+    moves.textContent = `Move: ${moveCount}`;
   };
 
   return {
     playRound,
     getActivePlayer,
+    getBoard: board.getBoard,
   };
 }
 
-const game = GameController();
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+  const dimension = 3;
+
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    for (let row = 0; row < dimension; row++) {
+      for (let column = 0; column < dimension; column++) {
+        const cellButton = document.createElement("button");
+        cellButton.dataset.row = row;
+        cellButton.dataset.column = column;
+        cellButton.className = "cell";
+        cellButton.textContent = board[row][column].getValue();
+        boardDiv.appendChild(cellButton);
+      }
+    }
+
+  };
+
+  function clickHandlerBoard(e) {
+    const selectedRow = e.target.dataset.row;
+    const selectedColumn = e.target.dataset.column;
+    if (!selectedRow || !selectedColumn) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  updateScreen();
+}
+
+ScreenController();
